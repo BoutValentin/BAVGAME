06674276f27d750524f493c63990f265221a4580
planckea <aurelien.plancke.etu@univ-lille.fr>
2021-03-15 23:26:26 +0100

    bugFix

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -86,7 +86,11 @@ export class HomePage extends Page {
 	handleScrollEvent() {
 		if (this.requestWasSend) return;
 		const element = document.documentElement;
-		if (element.scrollHeight - element.scrollTop !== element.clientHeight)
+		if (
+			element.scrollHeight - element.scrollTop !== element.clientHeight + 1 &&
+			element.scrollHeight - element.scrollTop !== element.clientHeight - 1 &&
+			element.scrollHeight - element.scrollTop !== element.clientHeight
+		)
 			return;
 		const elementToInsert = document.querySelector('header').parentNode;
 		const template = document.createElement('template');
